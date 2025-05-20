50ed874137518d9da20d40ca147cf85a95e6711f
jesusrs4705 <yisusrs2005@gmail.com>
2025-05-20 09:40:06 +0200

    Formato y errores notas

--- models/grade.py
+++ models/grade.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Grade(models.Model):
     _name = 'university.grade'
@@ -9,3 +10,9 @@ class Grade(models.Model):
     date = fields.Date(string='Grade Date', default=fields.Date.today, required=True)
     teacher_id = fields.Many2one('university.teacher', string='Grading Teacher', required=True, related='enrollment_id.teacher_id', store=True)
     value = fields.Float(string="Grade", digits=(3,2))
+
+    @api.constrains('value')
+    def _check_value(self):
+        for record in self:
+            if record.value < 0 or record.value > 10:
+                raise ValidationError('La nota debe estar entre 0 y 10.')
